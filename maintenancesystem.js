71acd324811b41a31906087ba6a53618b91499be
SniffGo <mcissell5115@gmail.com>
2026-04-06 14:15:51 -0500

    Update maintenancesystem.js

--- maintenancesystem.js
+++ maintenancesystem.js
@@ -1,4 +1,4 @@
-const openMaintenancePage = false; // Set to true to enable
+const openMaintenancePage = true; // Set to true to enable
 const maintenanceUrl = 'maintenance.html';
 
 // Only redirect if maintenance is ON and the user isn't already there
